a262e61f4c7d674e4a42efd1b7d203d0229ec569
Ermiyas Liyeh <eliyeh@u.rochester.edu>
2020-02-22 17:53:59 -0500

    Add files via upload

---
+++ CNF_converter.java
@@ -0,0 +1,15 @@
+
+public class CNF_converter {
+	
+	//have a cons...
+	//read the file
+	//determine each line prop
+	//set of int as a clause
+	//add set to a list  == converted cnf
+	//have const and meth for adding sets to cnf, getting sets from cnf, num of variables, get sign of an elem of a clause,
+	//variables for get var and get num of clause
+	
+	//TODO Ob Orie,...
+	//use cnf file
+	
+}
