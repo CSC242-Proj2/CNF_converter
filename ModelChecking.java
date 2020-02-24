a58d42ed21fa0546a17181643280a447999fd732
Emmanuel Debessa <44736066+emmanuelDebessa@users.noreply.github.com>
2020-02-24 17:24:45 -0500

    Add files via upload

---
+++ ModelChecking.java
@@ -0,0 +1,8 @@
+public class ModelChecking {
+//    • Given knowledge α and query β
+//      • For every possible world w
+//          • If α is satisfied by w
+//              • If β is not satisfied by w
+//                  • Conclude that α ⊭ β
+//      • Conclude that α ⊨ β
+}
