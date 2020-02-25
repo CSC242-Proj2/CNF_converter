03a580700244218589bf60963ac00a5e1775954a
Ermiyas Liyeh <eliyeh@u.rochester.edu>
2020-02-25 12:37:08 -0500

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -20,5 +20,39 @@ public class Operators {
         return true;
     }
 
+    public static Boolean negation(Boolean input) {
+		if(input == true) {
+			return false;
+		} else if(input == false) {
+			return true;
+		}
+		return null;
+	}
+	
+	public static Boolean or(Boolean[] inputs) {
+		Boolean returnVal = false;
+		
+		for(int i=0; i<inputs.length; i++) {
+			if(inputs[i] == true) {
+				returnVal = true;
+				break;
+			}
+		}
+		return returnVal;
+	}
+	
+	public static Boolean and(Boolean[] inputs) {
+		Boolean returnVal = true;
+		
+		for(int i=0; i<inputs.length; i++) {
+			if(inputs[i] == false) {
+				returnVal = false;
+				break;
+			}
+		}
+		
+		return returnVal;
+		
+	}
    
 }
