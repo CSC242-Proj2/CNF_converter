a281f5a4ae588b1b5fadb34601451ccbdd07768a
Emmanuel Debessa <44736066+emmanuelDebessa@users.noreply.github.com>
2020-02-25 12:39:03 -0500

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -21,38 +21,38 @@ public class Operators {
     }
 
     public static Boolean negation(Boolean input) {
-		if(input == true) {
-			return false;
-		} else if(input == false) {
-			return true;
-		}
-		return null;
-	}
-	
-	public static Boolean or(Boolean[] inputs) {
-		Boolean returnVal = false;
-		
-		for(int i=0; i<inputs.length; i++) {
-			if(inputs[i] == true) {
-				returnVal = true;
-				break;
-			}
-		}
-		return returnVal;
-	}
-	
-	public static Boolean and(Boolean[] inputs) {
-		Boolean returnVal = true;
-		
-		for(int i=0; i<inputs.length; i++) {
-			if(inputs[i] == false) {
-				returnVal = false;
-				break;
-			}
-		}
-		
-		return returnVal;
-		
-	}
-   
+        if(input) {
+            return false;
+        } else if(!input) {
+            return true;
+        }
+        return null;
+    }
+
+    public static Boolean or(Boolean[] inputs) {
+        Boolean returnVal = false;
+
+        for(int i=0; i<inputs.length; i++) {
+            if(inputs[i]) {
+                returnVal = true;
+                break;
+            }
+        }
+        return returnVal;
+    }
+
+    public static Boolean and(Boolean[] inputs) {
+        Boolean returnVal = true;
+
+        for(int i=0; i<inputs.length; i++) {
+            if(!inputs[i]) {
+                returnVal = false;
+                break;
+            }
+        }
+
+        return returnVal;
+
+    }
+
 }
